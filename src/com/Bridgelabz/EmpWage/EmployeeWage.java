ea101015c8adb1942f103f87463ff11f26468ac0
deepakphpweb <dipakrana2760@gmail.com>
2021-12-02 00:01:03 +0530

    Added by Rana : Check Calculate Daily Employee Wage in UseCase 2

--- src/com/Bridgelabz/EmpWage/EmployeeWage.java
+++ src/com/Bridgelabz/EmpWage/EmployeeWage.java
@@ -5,15 +5,22 @@ public class EmployeeWage {
     public void CheckEmpWage(){
         //Constants
         int IS_FULL_TIME = 1;
-        //Computation
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
 
+        //Computation
         double empCheck = Math.floor(Math.random() * 10) % 2 ;
 
         if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+            empHrs = 8;
         else
-            System.out.println("Employee is Absent");
+            empWage = 0;
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage : " + empWage);
     }
+
     public static void main(String[] args) {
 //        System.out.println("Welcome To Employee Wage Program");
 
