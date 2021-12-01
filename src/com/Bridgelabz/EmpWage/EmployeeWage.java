418b9ffba62541b33c08207d397e77291482d433
deepakphpweb <dipakrana2760@gmail.com>
2021-12-01 23:25:19 +0530

    Added by Rana : Check Employee Wage Present or Absent in UseCase 1

--- src/com/Bridgelabz/EmpWage/EmployeeWage.java
+++ src/com/Bridgelabz/EmpWage/EmployeeWage.java
@@ -1,7 +1,23 @@
 package com.Bridgelabz.EmpWage;
 
 public class EmployeeWage {
+
+    public void CheckEmpWage(){
+        //Constants
+        int IS_FULL_TIME = 1;
+        //Computation
+
+        double empCheck = Math.floor(Math.random() * 10) % 2 ;
+
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Program");
+//        System.out.println("Welcome To Employee Wage Program");
+
+        EmployeeWage ew = new EmployeeWage();
+        ew.CheckEmpWage();
     }
 }
